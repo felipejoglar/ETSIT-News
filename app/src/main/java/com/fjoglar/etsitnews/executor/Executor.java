6d5b35273e7266dec5f1d5187893562a8b3a8434
Felipe Joglar <felipejoglar@gmail.com>
2016-02-19 16:25:53 +0100

    005 - Added threading mechanism

---
+++ app/src/main/java/com/fjoglar/etsitnews/executor/Executor.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2016 Felipe Joglar Santos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fjoglar.etsitnews.executor;
+
+import com.fjoglar.etsitnews.interactor.UseCase;
+
+/**
+ * This executor is responsible for running interactors on background threads.
+ */
+public interface Executor {
+
+    /**
+     * This method should call the interactor's run method and thus start the
+     * interactor. This should be called on a background thread as interactors
+     * might do lengthy operations.
+     *
+     * @param interactor The interactor to run.
+     */
+    void execute(final UseCase interactor);
+
+}
---
+++ app/src/main/java/com/fjoglar/etsitnews/executor/MainThread.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 Felipe Joglar Santos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fjoglar.etsitnews.executor;
+
+/**
+ * This interface will define a class that will enable interactors to run certain
+ * operations on the main (UI) thread.
+ * For example, if an interactor needs to show an object to the UI this can be used
+ * to make sure the show method is called on the UI thread.
+ */
+public interface MainThread {
+
+    /**
+     * Make runnable operation run in the main thread.
+     *
+     * @param runnable The runnable to run.
+     */
+    void post(final Runnable runnable);
+
+}
